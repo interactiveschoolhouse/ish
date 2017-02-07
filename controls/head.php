611c7b49cb71fd193f2e08fda29047d0c83e3a54
Jeff Roy <Jeff Roy>
2017-02-07 05:44:18 -0500

    adding less for styles

--- controls/head.php
+++ controls/head.php
@@ -20,7 +20,7 @@
   <link href="/css/style.css" rel="stylesheet">
   <link rel="stylesheet" href="/css/colors/default.css" id="option_color">
   <link rel="stylesheet" href="/css/themeoverrides.css">
-  <link rel="stylesheet" href="/css/mysite.css">
+  <link rel="stylesheet" href="/css/site.css">
 
   <!-- Icons -->
   <link rel="shortcut icon" href="/img/favicon.png">
