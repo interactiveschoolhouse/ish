433b756f7147e2313b8fcd3e65bdd3d1824c503c
Jeff Roy <Jeff Roy>
2017-03-05 10:37:56 -0500

    removing unused assets from theme

--- controls/head.php
+++ controls/head.php
@@ -7,10 +7,8 @@
   <title><?php echo $pageTitle ?></title>
   
   <!-- PLUGINS CSS STYLE -->
-  <link href="/plugins/jquery-ui/jquery-ui.css" rel="stylesheet">
   <link href="/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="/plugins/selectbox/select_option1.css">
   
   <!-- GOOGLE FONT -->
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
@@ -22,13 +20,6 @@
   <link rel="stylesheet" href="/css/site.min.css">
 
   <!-- Icons -->
-  <link rel="shortcut icon" href="/img/favicon.png">
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+  <!--<link rel="shortcut icon" href="/img/favicon.png">-->
 
 </head>
