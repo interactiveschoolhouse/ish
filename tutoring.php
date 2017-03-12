0cef5a32112806887a4250416194741818a4c746
Jeff Roy <Jeff Roy>
2017-03-12 13:29:51 -0400

    adding tutoring page with images

---
+++ tutoring.php
@@ -0,0 +1,63 @@
+<?php 
+    $pageTitle="Tutoring";
+    require "/code/startup.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php require "/controls/head.php" ?>
+<body class="body-wrapper version3">
+  <div class="main-wrapper">
+    <?php require "/controls/header.php" ?>
+    <section class="top-content">
+        <div class="container">
+            <h2>Tutoring</h2>
+            <h3>Tutoring in math, reading comprehension, and writing skills.<h3>
+            <p>
+                Every student learns a different way. No matter how well his or her teacher explains the material, some students just need more time to practice the skill. Interactive School House provides a supportive environment for students who need this time to be guided in the right direction.
+            </p>
+            <h3>
+                Interactive School House Can Offer You or Your Child Tutors Who:
+            </h3>
+            <ul class="bulleted-list">
+                <li>are responsive to students’ individual needs and can maximize their learning potential via flexible teaching strategies. </li>
+                <li>build on students’ current interests and encourage them to discover new interests.</li>
+                <li>guide students through challenging materials, and show them how to work through their confusion.</li>
+                <li>match the complexity of work to the students’ readiness to complete the task.</li>
+                <li>connect what students already know to what they are learning.</li>
+                <li>offer different modes of learning tailored to the learning style of the student.</li>
+            </ul>
+            <h3>
+                Coaching in Study Skills include:
+            </h3>
+            <ul class="bulleted-list">
+                <li>How to apply appropriate strategies.</li>
+                <li>How to construct and use graphic representations.</li>
+                <li>How to summarize information.</li>
+                <li>How to make reading notes.</li>
+                <li>How to conduct and report research.</li>
+            </ul>
+            <h3>
+                About Our Tutoring Space
+            </h3>
+            <div class="tutoring__gallery">
+                <div class="tutoring-gallery__left-panel">
+                    <img src="/images/common/nancy-roy-tutoring-1.jpg">
+                </div>
+                <div class="tutoring-gallery__right-panel">
+                    <img src="/images/common/art-1.jpg">
+                    <img src="/images/common/art-2.jpg">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php require "/controls/footer.php" ?>
+  </div>
+    
+  <?php require "/controls/jsLibraries.php";?>
+  <script src="/js/register.js"></script>
+</body>
+<!-- 
+    Maybe need to turn on auto-return?
+    http://stackoverflow.com/questions/18007480/paypal-checkout-return-data
+-->
